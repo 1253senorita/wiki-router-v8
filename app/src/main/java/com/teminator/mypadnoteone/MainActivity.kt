c82cb85fbc0bb93275c2dbbcee579f747c45de03
1253senorita <136488814+1253senorita@users.noreply.github.com>
2026-07-29 09:05:17 +0900

    Initial commit 또는 프로젝트 2026.07.29.09ViewBinding 적용 및 기초 환경 세팅

--- app/src/main/java/com/teminator/mypadnoteone/MainActivity.kt
+++ app/src/main/java/com/teminator/mypadnoteone/MainActivity.kt
@@ -5,13 +5,25 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.teminator.mypadnoteone.databinding.ActivityMainBinding // ★ ViewBinding 클래스 import
 
 class MainActivity : AppCompatActivity() {
+
+    // 1. 바인딩 객체 변수 선언
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+
+        // 2. XML을 인플레이트해서 바인딩 객체 생성
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        // 3. R.layout... 대신 binding.root를 화면에 설정
+        setContentView(binding.root)
+
+        // 4. findViewById(R.id.main) 대신 binding.main으로 깔끔하게 접근!
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
